03ca6de25e70f4ee5538d5e6ef3af32eeb68025c
lovroselic <lovro.selic@gmail.com>
2024-07-01 14:10:43 +0200

    asset preparation

---
+++ Assets/Definitions/CastleHaunt2/assets_CastleHaunt2.js
@@ -0,0 +1,10 @@
+/*jshint browser: true */
+/*jshint -W097 */
+/*jshint -W117 */
+/*jshint -W061 */
+//Assets for CastleHaunt2
+"use strict";
+console.log("%cAssets for CastleHaunt started.", "color: orange");
+
+
+console.log("%cAssets for CastleHaunt ready.", "color: orange");
